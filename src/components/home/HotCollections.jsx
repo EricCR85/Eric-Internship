843b082b88ebdd3fb97c1a1a459e66ed97f9082e
EricCR85 <ecr.ceo@gmail.com>
2026-05-18 15:05:33 -0400

    update

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+// import the carousel component
+import slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const HotCollections = () => {
   // // Create the state to store our dynamic collections array
   const [collections, setCollections] = useState([]);
 
-  // Fetch data from the API endpoint when the component mounts 
+  // Fetch data from the API endpoint when the component mounts
   useEffect(() => {
     const fetchCollections = async () => {
-      try{
+      try {
         const response = await axios.get(
-          "https://us-centrali-nft-cloud-functions.cloudfunctions.net/hotCollections"
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections",
         );
         // save array of 6 nft items into state
         setCollections(response.data);
@@ -23,6 +25,32 @@ const HotCollections = () => {
     };
     fetchCollections();
   }, []);
+
+  // Carousel settings config
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+    ],
+  };
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -34,30 +62,42 @@ const HotCollections = () => {
             </div>
           </div>
           {/* map over the dynamic collections data instead of the repeating hardcode elements */}
-
-          {collections.map((nft) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={nft.id}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nft.nftImage} className="lazy img-fluid" alt={nft.title} />
-                  </Link>
+          <div className="col-lg-12">
+            {/* wrap dynamic mapping logic inside the slider wrapper */}
+            <slider {...settings}>
+              {collections.map((nft) => (
+                <div className="carousel-item-padding" key={nft.id}>
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                      <Link to="/item-details">
+                        <img
+                          src={nft.nftImage}
+                          className="lazy img-fluid"
+                          alt={nft.title}
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Link to="/author">
+                        <img
+                          className="lazy pp-coll"
+                          src={nft.authorImage}
+                          alt=""
+                        />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <Link to="/explore">
+                        <h4>{nft.title}</h4>
+                      </Link>
+                      <span>ERC-{nft.code}</span>
+                    </div>
+                  </div>
                 </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={nft.authorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{nft.title}</h4>
-                  </Link>
-                  <span>ERC-{nft.code}</span>
-                </div>
-              </div>
-            </div>
-))}
+              ))}
+            </slider>
+          </div>
         </div>
       </div>
     </section>
