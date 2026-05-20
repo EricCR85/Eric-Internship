814912e8973ebc827a56a26056e57033bde06b44
EricCR85 <ecr.ceo@gmail.com>
2026-05-20 17:22:05 -0400

    update

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react"; 
 import { Link } from "react-router-dom";
-import axios from "axios";
-import Skeleton from "../UI/Skeleton";
+
+// import axios from "axios";
+// import Skeleton from "../UI/Skeleton";
 
 const AuthorItems = ({ authorItems }) => {
   const items = authorItems || [];
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
   return (
     <div className="de_tab_content">
@@ -67,50 +68,4 @@ const AuthorItems = ({ authorItems }) => {
 
 export default AuthorItems;
 
-// useEffect(() => {
-//   const fetchAuthorItems = async () => {
-//     try {
-//       const response = await axios.get(
-//         `https://us-central1-nft-cloud-functions.cloudfunctions.net/authorItems?authorId=${authorId}`
-//       );
-//       setAuthorItems(response.data);
-//       setLoading(false);
-//     } catch (error) {
-//       console.error("Error fetching author items:", error);
-//       setLoading(false);
-//     }
-//   };
 
-//   fetchAuthorItems();
-// }, [authorId]);
-
-// return (
-//   <div className="de_tab_content">
-//     <div className="tab-1">
-//       <div className="row">
-//         {loading
-//           ? new Array(8).fill(0).map((_, index) => (
-//               <div
-//                 className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
-//               >
-//                 key={index}
-//                 <div className="nft__item">
-//                   <div className="author_list_pp">
-//                     <Skeleton width="50px" height="50px" borderRadius="50%" />
-//                     <i className="fa fa-check"></i>
-//                   </div>
-//                   <div className="nft__item_wrap">
-//                     <Skeleton width="100%" height="200px" />
-//                   </div>
-//                   <div className="nft__item_info">
-//                     <Skeleton width="100px" height="20px" />
-//                     <div className="nft__item_price">
-//                       <Skeleton width="60px" height="20px" />
-//                     </div>
-//                     <div className="nft__item_like">
-//                       <Skeleton width="40px" height="20px" />
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             )) :
