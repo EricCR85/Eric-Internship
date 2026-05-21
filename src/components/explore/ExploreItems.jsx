1eda5353d3ab77b5ad0b1616af6f3651e9ef8f54
EricCR85 <ecr.ceo@gmail.com>
2026-05-21 11:40:38 -0400

    update

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -8,113 +8,110 @@ const Countdown = ({ expiryDate }) => {
 
   useEffect(() => {
     if (!expiryDate) return;
-  
-  const calculateTimeLeft = () => {
-    const now = new Date().getTime();
-    const distance = new Date(expiryDate).getTime() - now;
 
-    if (distance < 0) {
-      return "Expired";
-    }
-          const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-          const hours = Math.floor(
-            (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-          );
-          const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-          const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-          return `${days}d ${hours}h ${minutes}m ${seconds}s`;
-    }
+    const calculateTimeLeft = () => {
+      const now = new Date().getTime();
+      const distance = new Date(expiryDate).getTime() - now;
 
+      if (distance < 0) {
+        return "Expired";
+      }
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    };
+
+    setTimeLeft(calculateTimeLeft());
+    const timer = setInterval(() => {
       setTimeLeft(calculateTimeLeft());
-      const timer = setInterval(() => {
-        setTimeLeft(calculateTimeLeft());
-      }, 1000)
-    
-        return () => clearInterval(timer);
-      }, [expiryDate]);
-    
+    }, 1000);
 
+    return () => clearInterval(timer);
+  }, [expiryDate]);
 
   return <div className="de_countdown">{timeLeft}</div>;
 };
 
-const ExploreItems = ({ items, visibleCount, laoding }) => {
+const ExploreItems = ({ items, visibleCount, loading }) => {
   if (loading) {
     return (
       <>
-      {new Array(8).fill(0).map((_, index) => (
-        <div key={index} className="d_item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-        style={{ display: "black", backgroundSize: "cover"}}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Skeleton width="50px" height="50px" borderRadius="50%" />
+        {new Array(8).fill(0).map((_, index) => (
+          <div
+            key={index}
+            className="d_item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+            style={{ display: "black", backgroundSize: "cover" }}
+          >
+            <div className="nft__item">
+              <div className="author_list_pp">
+                <Skeleton width="50px" height="50px" borderRadius="50%" />
               </div>
               <div className="nft__item_wrap">
                 <Skeleton width="100%" height="250px" borderRadius="8px" />
-                </div>
-                <div className="nft__item_info">
-                  <Skeleton width="100px" height="20px" />
-                  <Skeleton width="60px" height="150px" />
+              </div>
+              <div className="nft__item_info">
+                <Skeleton width="100px" height="20px" />
+                <Skeleton width="60px" height="150px" />
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
       </>
     );
   }
 
   return (
     <>
-    {items &&
-      {items.slice(0, visibleCount).map((item, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }} 
+      {items &&
+        items.slice(0, visibleCount).map((item, index) => (
+          <div
+            key={index}
+            className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+            style={{ display: "block", backgroundSize: "cover" }}
           >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link to={`/author/${item.authorId}`}>
-              data-bs-toggle="tooltip"
-              data-bs-placement="top"
-              
-
-              <img className="lazy" src={item.authorImage} alt="" />
-              <i className="fa fa-check"></i>
-              </Link>
-              </div>
-            {item.expiryDate && <Countdown expiryDate={item.expiryDate} />}
-
-            <div className="nft__item_wrap">
-            <div className="nft__item_buttons">
-            <i className="fa fa-shopping-basket"></i>
-            </div>
-            <Link to={`/item-details/${item.nftId}`}>
-            <img src={item.nftImage}
-            className="lazy nft__item_preview"
-            alt=""
-            />
-            </Link>
-            </div>
-            <div className="nft__item_info">
-            <Link to={`/item-details/${item.nftId}`}>
-            <h4>{item.title}</h4>
-            </Link>
-            <div className="nft__item_price">{item.price} ETN</div>
-            <div className="nft__item_like">
-            <i className="fa fa-heart"></i>
-            <span>{item.likes}</span>
-            </div>
-              </div>
-              </div>
-              </div>
-      ))}
-    }
-      </>
-    );
-  };
-
+            <div className="nft__item">
+              <div className="author_list_pp">
+                <Link
+                  to={`/author/${item.authorId}`}
                 
+                >
+                  <img className="lazy" src={item.authorImage} alt="" />
+                  <i className="fa fa-check"></i>
+                </Link>
+              </div>
+              {item.expiryDate && <Countdown expiryDate={item.expiryDate} />}
+
+              <div className="nft__item_wrap">
+                <div className="nft__item_buttons">
+                  <i className="fa fa-shopping-basket"></i>
+                </div>
+                <Link to={`/item-details/${item.nftId}`}>
+                  <img
+                    src={item.nftImage}
+                    className="lazy nft__item_preview"
+                    alt=""
+                  />
+                </Link>
+              </div>
+              <div className="nft__item_info">
+                <Link to={`/item-details/${item.nftId}`}>
+                  <h4>{item.title}</h4>
+                </Link>
+                <div className="nft__item_price">{item.price} ETN</div>
+                <div className="nft__item_like">
+                  <i className="fa fa-heart"></i>
+                  <span>{item.likes}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+    </>
+  );
+};
 
 export default ExploreItems;
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -6,26 +6,40 @@ import ExploreItems from "../components/explore/ExploreItems";
 const Explore = () => {
   const [items, setItems] = useState([]);
   const [visibleCount, setVisibleCount] = useState(8);
-
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     window.scrollTo(0, 0);
     fetchData();
   }, []);
 
-  const fetchData = async () => {
+  const fetchData = async (filterValue) => {
+    setLoading(true);
     try {
       const { data } = await axios.get(
-        "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore",
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore${
+          filterValue ? `?filter=${filterValue}` : ""
+        }`,
       );
-      console.log(data)
-      
+
+      // console.log(data)
+
       setItems(data);
     } catch (error) {
       console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
-  
+  const handleFilterChange = (event) => {
+    const selectedFilter = event.target.value;
+    fetchData(selectedFilter);
+  };
+
+  const loadMoreItems = () => {
+    setVisibleCount((prevCount) => prevCount + 4);
+  };
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -51,12 +65,37 @@ const Explore = () => {
         <section aria-label="section">
           <div className="container">
             <div className="row">
-              <ExploreItems items={items.slice(0, visibleCount)} />
+              <div className="col-md-12 text-center">
+                <div className="items_filter">
+                  <select
+                    id="filter-items"
+                    name="filter-items"
+                    onChange={handleFilterChange}
+                    defaultValue=""
+                  >
+                    <option value="">Default</option>
+                    <option value="price_low_to_high">
+                      Price, Low to High
+                    </option>
+                    <option value="price_high_to_low">
+                      Price, High to Low
+                    </option>
+                    <option value="Likes_high_to_low">Most Liked</option>
+                  </select>
+                </div>
+              </div>
+              <ExploreItems
+                items={items}
+                visibleCount={visibleCount}
+                loading={loading}
+              />
+
               {visibleCount < items.length && (
                 <div className="col-md-12 text-center">
                   <button
+                    onClick={loadMoreItems}
+                    id="loadmore"
                     className="btn-main"
-                    onClick={() => setVisibleCount(visibleCount + 4)}
                   >
                     Load More
                   </button>
@@ -69,5 +108,4 @@ const Explore = () => {
     </div>
   );
 };
-
 export default Explore;
